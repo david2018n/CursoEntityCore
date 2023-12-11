3ce90dd7e27830a292939dde34f45d96a444da69
JUAN DAVID NUSTES <jnustesn@contratista.epm.co>
2023-12-11 18:55:59 -0500

    Adicion de nugets y creacion de DbContext

---
+++ CursoEntityCore/Datos/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿
+namespace CursoEntityCore.Datos
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> opciones) : base(opciones)
+        {
+            
+        }
+
+        //Modelos de las entidades
+    }
+}
